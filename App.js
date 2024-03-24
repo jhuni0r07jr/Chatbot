17d40f7c6d53487a4eb1b066cfb5cde8f491fc0d
Antônio Júnior <Jhuniortotal000@gmail.com>
2024-03-24 14:06:33 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const output = document.getElementById("output");
+    const userInput = document.getElementById("user-input");
+    const sendBtn = document.getElementById("send-btn");
+  
+    sendBtn.addEventListener("click", function() {
+      const userMessage = userInput.value;
+      output.innerHTML += `<div><strong>Você:</strong> ${userMessage}</div>`;
+      userInput.value = "";
+  
+      // Simular resposta do chatbot (pode ser adaptado para respostas dinâmicas)
+      setTimeout(function() {
+        output.innerHTML += `<div><strong>Bot:</strong> Olá! Como posso ajudá-lo?</div>`;
+        output.innerHTML += `<div><strong>Bot:</strong> Faturas</div>`;
+        output.innerHTML += `<div><strong>Bot:</strong> Orçamento</div>`;
+        output.innerHTML += `<div><strong>Bot:</strong> Conhecer nossos serviços</div>`;
+        output.innerHTML += `<div><strong>Bot:</strong> Falar com atendente</div>`;
+      }, 500);
+
+/*      setTimeout(function() {
+        window.location.href = "http://127.0.0.1:5500/index.html";
+      }, 2000);*/
+      
+    });
+});  
